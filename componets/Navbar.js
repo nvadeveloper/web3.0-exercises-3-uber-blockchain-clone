fd546874d594606cffb90d6acdc04c41caebc6f3
Nikita <nvadeveloper@gmail.com>
2022-03-25 19:43:45 +0300

    start

---
+++ componets/Navbar.js
@@ -0,0 +1,13 @@
+const style = { 
+    wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center px-60 z-20`
+}
+
+const Navbar = () => {
+    return (
+        <div className={style.wrapper}>
+            Navbar
+        </div>
+    )
+}
+  
+export default Navbar
---
+++ pages/index.js
@@ -0,0 +1,27 @@
+// const style = {
+//   wrapper: ``,
+// }
+
+import Navbar from "../componets/Navbar"
+
+const style = {
+  wrapper: `h-screen w-screen flex flex-col`
+}
+
+const Home = () => {
+  return (
+    <div className={style.wrapper}>
+      <Navbar />
+      <div className={style.main}>
+      
+      </div>
+      <div className={style.rideRequestContainer}>
+        <div className={style.rideRequest}>
+        
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
