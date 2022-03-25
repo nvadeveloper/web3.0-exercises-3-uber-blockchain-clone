0a53099bdda7b4914d98873c8cdb1adf49c9db04
Nikita <nvadeveloper@gmail.com>
2022-03-25 21:19:08 +0300

    add map

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,17 +3,17 @@ import userImage from '../temp/userImage.png'
 import { BsPerson } from 'react-icons/bs'
 
 
-const style = { 
-    wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center px-60 z-20`,
+const style = {
+    wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center px-60 fixed z-20`,
     leftMenu: `flex gap-3`,
-    rightMenu: `flex gap-3 items-center`,
     logo: `text-3xl text-white flex cursor-pointer mr-16`,
     menuItem: `text-lg text-white font-medium flex items-center mx-4 cursor-pointer`,
+    rightMenu: `flex gap-3 items-center`,
     userImageContainer: `mr-2`,
     userImage: `h-10 w-10 mr-4 rounded-full p-px object-cover cursor-pointer`,
     loginButton: `flex items-center cursor-pointer rounded-full hover:bg-[#333333] px-4 py-1`,
-    loginText: `ml-2`
-}
+    loginText: `ml-2`,
+  }
 
 const currentAccount = ''
 
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,15 @@
 //   wrapper: ``,
 // }
 
+import Map from "../components/Map"
 import Navbar from "../components/Navbar"
 
 const style = {
-  wrapper: `h-screen w-screen flex flex-col`
+  wrapper: `h-screen w-screen flex flex-col`,
+  main: `h-full w-screen flex-1 z-10`,
+  mapContainer: `flex-1 w-full h-full`,
+  rideRequestContainer: `h-full w-[400px] ml-[1rem] py-[3rem] absolute top-0 left-0 flex flex-col justify-end z-20`,
+  rideRequest: `h-full max-h-[700px] bg-white rounded-lg flex flex-col overflow-scroll`,
 }
 
 const Home = () => {
@@ -13,7 +18,7 @@ const Home = () => {
     <div className={style.wrapper}>
       <Navbar />
       <div className={style.main}>
-      
+        <Map />
       </div>
       <div className={style.rideRequestContainer}>
         <div className={style.rideRequest}>
